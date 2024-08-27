a63048e40dad5c5dfcb8ff0000271783215e967b
Harsh Pandhe <harshpandhehome@gmail.com>
2024-08-27 16:52:09 +0530

    Functions Understood

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let sentence: String = String::from("the quick brown fox jumps over the lazy d  og");
+    let sentence: String = String::from("the quick brown fox jumps over the lazy dog");
     println!("{}", sentence);
 
     let first_word: String = get_first_word(sentence);
@@ -7,7 +7,9 @@ fn main() {
     for _i in 0..a {
         println!("Harsh was here!");
     }
-    print!("{}", first_word);
+    println!("First Word is: {}", first_word);
+    let sum: i32 = do_sum(10, 20);
+    print!("Sum is: {}", sum);
 }
 
 fn get_first_word(sentence: String) -> String {
@@ -19,4 +21,8 @@ fn get_first_word(sentence: String) -> String {
         }
     }
     return ans;
+}
+
+fn do_sum(i: i32, j: i32) -> i32 {
+    return i + j;
 }
