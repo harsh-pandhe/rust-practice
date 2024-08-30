0a97ae62c14a14b5180532aea2ede39683ccb9ce
Harsh Pandhe <harshpandhehome@gmail.com>
2024-08-31 02:00:55 +0530

    Ownership 01

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let s1 = String::from("hello");
-    let s2 = s1;
-    // println!("{}", s1); // error: value used here after move
-    println!("{}", s2);
+    let my_string = String::from("Hello, world!");
+    // takes_ownership(my_string); // This will not work because my_string has been moved to takes_ownership function
+    // println!("{}", my_string); // This will not work because my_string has been moved to takes_ownership function
+    takes_ownership(my_string.clone()); // This will work because my_string has been cloned
+    println!("{}", my_string); // This will work because my_string has been cloned
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
 }
