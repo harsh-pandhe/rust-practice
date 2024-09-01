2a64ca90bedecfaeee4f568458e0e470183c5256
Harsh Pandhe <harshpandhehome@gmail.com>
2024-09-02 00:21:38 +0530

    Reference 01

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,7 @@
-/**
 fn main() {
-    let my_string = String::from("Hello, world!");
-    // takes_ownership(my_string); // This will not work because my_string has been moved to takes_ownership function
-    // println!("{}", my_string); // This will not work because my_string has been moved to takes_ownership function
-    takes_ownership(my_string.clone()); // This will work because my_string has been cloned
-    println!("{}", my_string); // This will work because my_string has been cloned
-}
+    let s1 = String::from("hello");
+    let s2 = &s1;
 
-
-fn takes_ownership(some_string: String) {
-    println!("{}", some_string);
-}
-
- */
-
-// fn main() {
-//     let my_string = String::from("Hello, world!");
-//     let my_string = takes_ownership(my_string);
-//     println!("{}", my_string);
-// }
-
-// fn takes_ownership(some_string: String) -> String {
-//     println!("{}", some_string);
-//     return some_string; // This will return the value of some_string to the caller
-// }
-
-fn main() {
-    let my_string = String::from("Hello, world!");
-    let my_string_02 = takes_ownership(my_string);
-    println!("{}", my_string_02);
-    // let mut my_string = String::from("Hello, world!");
-    // let my_string = takes_ownership(my_string);
-    // println!("{}", my_string);
-}
-
-fn takes_ownership(some_string: String) -> String {
-    println!("{}", some_string);
-    return some_string;
+    println!("s2: {}", s2);
+    println!("s1: {}", s1);
 }
