3a17250f1f54e1ca564b86bc904e4d29a3fe1164
Harsh Pandhe <harshpandhehome@gmail.com>
2024-09-02 00:32:37 +0530

    Reference 02

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 fn main() {
     let s1 = String::from("hello");
-    let s2 = &s1;
+    takes_ownership(&s1); // s1 is borrowed
+    println!("{}", s1); // s1 is borrowed, so it can be used here
 
-    println!("s2: {}", s2);
-    println!("s1: {}", s1);
+    let s2 = &s1;
+    let s3 = &s1;
+    let s4 = &s1;
+
+    println!("{}, {}, {}", s2, s3, s4);
+    println!("{}", s1); // s1 is borrowed, so it can be used here
+}
+
+fn takes_ownership(s: &String) {
+    // s is a reference to a String
+    println!("{}", s);
 }
