2e23802296abf140be919471c846113d294a2858
Harsh Pandhe <harshpandhehome@gmail.com>
2024-09-02 01:41:20 +0530

    Struct 02

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
-struct User {
-    active: bool,
-    email: String,
-    username: String,
-    sign_in_count: u64,
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+         return self.width * self.height
+    }
 }
 
 fn main() {
-    let user1 = User {
-        email: String::from("harshpandhehome@gmail.com"),
-        username: String::from("harshpandhe"),
-        active: true,
-        sign_in_count: 1,
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
     };
-    println!("User1 email: {}", user1.email);
-    println!("User1 username: {}", user1.username);
-    println!("User1 active: {}", user1.active);
-    println!("User1 sign_in_count: {}", user1.sign_in_count);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
