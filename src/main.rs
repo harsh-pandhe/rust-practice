fe74ecb873003ad3b17cfee6a17230043bc00d54
Harsh Pandhe <harshpandhehome@gmail.com>
2024-09-03 01:33:23 +0530

    Error Handling 02

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-struct Point<T> {
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
 }
 
 fn main() {
-    let integer = Point { x: 5, y: 10 };
+    let integer = Point { x: 5, y: 10.0 };
     let float = Point { x: 1.0, y: 4.0 };
-
+    let string = Point { x: "Hello", y: "World" };
     println!("integer.x = {}, integer.y = {}", integer.x, integer.y);
     println!("float.x = {}, float.y = {}", float.x, float.y);
+    println!("string.x = {}, string.y = {}", string.x, string.y);
 }
