4222ac24596aadb4b96e3a8e53ec48a9e1f08b5a
Harsh Pandhe <harshpandhehome@gmail.com>
2024-09-03 01:40:41 +0530

    Revert "Error Handling 02"
    This reverts commit a7829b57c23bad63ad3a3dd04fdcd763049d5f08.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use std::fs;
-
-enum Result<A, B> {
-    Ok(A),
-    Err(B),
+struct Point<T, U> {
+    x: T,
+    y: U,
 }
 
 fn main() {
-    fs::read_to_string("src/main.rs").map(|contents| {
-        println!("{}", contents);
-    })
-    .map_err(|err| {
-        eprintln!("Error: {}", err);
-    });
+    let integer = Point { x: 5, y: 10.0 };
+    let float = Point { x: 1.0, y: 4.0 };
+    let string = Point { x: "Hello", y: "World" };
+    println!("integer.x = {}, integer.y = {}", integer.x, integer.y);
+    println!("float.x = {}, float.y = {}", float.x, float.y);
+    println!("string.x = {}, string.y = {}", string.x, string.y);
 }
