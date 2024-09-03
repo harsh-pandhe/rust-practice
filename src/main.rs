58ec0728180368b9a3e2febf5d65b128a976b2cd
Harsh Pandhe <harshpandhehome@gmail.com>
2024-09-04 01:05:40 +0530

    Error Handling 05

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 use std::fs;
 
-enum Result<A, B> {
-    Ok(A),
-    Err(B),
-}
-
 fn main() {
     let res = fs::read_to_string("src/main.rs");
-
         match res {
             Ok(content) => {
                 println!("{}", content);
